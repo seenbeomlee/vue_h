ce5e94b8dc7436ca1d49ada295e2ea693e062259
Seenbeom Lee <mail@seenblee.com>
2020-07-07 11:21:33 +0900

    1.20-2: practice: vue instance life-cycle

--- test1/src/views/Home.vue
+++ test1/src/views/Home.vue
@@ -20,41 +20,51 @@ export default {
     };
   },
 
+  /* these are all hook for life-cycle */
+  /* when vue instance is being made or eliminated, this life-cycle hook is also progressed */
+
   beforeCreate() {
-    /* console.log("beforeCreate", this.name); */
     alert("beforeCreate");
+    /* cant use Data(this.title...), Method() and events(vm$on, vm.$emit...) cause they are not setted yet */
   },
 
   created() {
-    /* console.log("created", this.name); */
     alert("Created");
+    /* you can track the data and computed, methods, watch ... are valid */
+    /* but you can't use $el yet. because DOM does not mounted yet  */
   },
 
   beforeMount() {
     alert("beforeMount");
-    /* if you refresh the browser */
+    /* before the first rendering(DOM). this.$el doesn't exist yet */
   },
 
   mounted() {
     alert("Mount");
-    /* control DOM, it shows display */
+    /* after create the DOM. it can approach all elements */
   },
 
   beforeUpdate() {
     alert("beforeUpdate");
+    /* if you change the value of some data in this section, it does not call the beforeUpdate hook. */
   },
 
   updated() {
     alert("updated");
     /* if you clicked the button, update is being progressed and display an alert */
+
+    /* after re-rendering(DOM) */
+    /* if you change data in this hook, it can make a infinite loop */
   },
   beforeDestroy() {
     alert("beforeDestroy");
-    /* why it does not work?! == nope, i just wrote it 'beforeDestoy' :D */
+    /* before eliminate the vue instance. so you can approach all instance. */
   },
   destroyed() {
     alert("destroyed");
     /* if you change the view-router to About */
+
+    /* after elimanate the vue instance */
   },
   methods: {
     updateName() {
